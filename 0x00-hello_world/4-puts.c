fa0981ee7792b3703c74c32bf0c6c36e10a3d456
Olajide33 <oluponmile_olajide@yahoo.com>
2023-02-16 10:54:03 -0800

    Task04

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * Program that prints a line with puts function
+ * Return: 0
+ */
+int main(void)
+{
+puts("\n"Programming is like building a multilingual puzzle");
+return (0);
+}
